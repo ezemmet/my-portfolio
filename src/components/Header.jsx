11f59ac8568f5bd90da01708f26f7549ef39ee52
techienicke <developernick.e@gmail.com>
2024-11-04 22:19:04 +0530

    Responsive check

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,37 @@
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 const Header = () => {
 
   const ezemmetLogo = '<ezemmet/>'
+  const eLogo = '<e/>'
 
-  const [menu, setMenu] = useState('right-[-50%]')
+  const [menu, setMenu] = useState('hidden')
 
   const handleToggleMenu = () => {
-    if (menu === 'right-[-50%]') {
-      setMenu('right-[0%]')
+    if (menu === 'hidden') {
+      setMenu('block')
     }
     else {
-      setMenu('right-[-50%]')
+      setMenu('hidden')
     }
   }
 
   return (
     <div className='grid grid-cols-2 items-center h-[10vh] bg-[#0c0c0c] text-white px-[3rem] lg:px-[10rem]'>
       <div>
-        <p className='text-2xl font-fontPrompt text-red-600'>{ezemmetLogo}</p>
+        <p className='text-2xl font-fontPrompt text-red-600 hidden md:block'>{ezemmetLogo}</p>
+        <p className='text-2xl font-fontPrompt text-red-600 block md:hidden'>{eLogo}</p>
       </div>
       <div className='text-end'>
         <FontAwesomeIcon onClick={handleToggleMenu} icon={faBars} className='md:hidden text-2xl cursor-pointer' />
-        <div className={`absolute transition-right duration-100 ease-in-out ${menu} md:relative md:block md:right-0`}>
-          <ul className='md:flex justify-end font-fontKanit bg-black'>
-            <li className='px-5'><a href="/home">Home</a></li>
-            <li className='px-5'><a href="/about">About</a></li>
-            <li className='px-5'><a href="/resume">Resume</a></li>
-            <li className='px-5'><a href="/contact">Contact</a></li>
+        <div className={`absolute right-0 top-[3.8rem] md:top-0 ${menu} md:relative md:block`}>
+          <ul className='md:flex text-center md:justify-end font-fontKanit bg-[#000000d7]'>
+            <li className='px-5 hover:text-red-600'><a href="/home">Home</a></li>
+            <li className='px-5 hover:text-red-600'><a href="/about">About</a></li>
+            <li className='px-5 hover:text-red-600'><a href="/resume">Resume</a></li>
+            <li className='px-5 hover:text-red-600'><a href="/contact">Contact</a></li>
           </ul>
         </div>
       </div>
