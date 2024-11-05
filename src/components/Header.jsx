b5a91bf9c2f7cecdb48e314d1b1daef7591c0be7
techienicke <developernick.e@gmail.com>
2024-11-05 15:44:50 +0530

    Start home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,15 +19,15 @@ const Header = () => {
   }
 
   return (
-    <div className='grid grid-cols-2 items-center h-[10vh] bg-[#0c0c0c] text-white px-[3rem] lg:px-[10rem]'>
+    <div className='grid grid-cols-2 items-center h-[10vh] bg-[#000000] text-white px-[3rem] lg:px-[10rem]'>
       <div>
         <p className='text-2xl font-fontPrompt text-red-600 hidden md:block'>{ezemmetLogo}</p>
         <p className='text-2xl font-fontPrompt text-red-600 block md:hidden'>{eLogo}</p>
       </div>
       <div className='text-end'>
         <FontAwesomeIcon onClick={handleToggleMenu} icon={faBars} className='md:hidden text-2xl cursor-pointer' />
-        <div className={`absolute right-0 top-[3.8rem] md:top-0 ${menu} md:relative md:block`}>
-          <ul className='md:flex text-center md:justify-end font-fontKanit bg-[#000000d7]'>
+        <div className={`absolute right-0 top-[10%] md:top-0 ${menu} md:relative md:block`}>
+          <ul className='md:flex text-center md:justify-end font-fontKanit bg-[#000000d7] md:bg-transparent'>
             <li className='px-5 hover:text-red-600'><a href="/home">Home</a></li>
             <li className='px-5 hover:text-red-600'><a href="/about">About</a></li>
             <li className='px-5 hover:text-red-600'><a href="/resume">Resume</a></li>
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>
-      home
+    <div className='bg-[#0e0e0e] h-[90vh] grid md:grid-cols-2 px-[3rem] text-[#fff]'>
+      <div className='border border-red-600 px-[5rem] justify-center flex flex-col'>
+        <p className='text-red-500 text-xl'>Hello, My Name Is</p>
+        <h2 className='text-4xl'>NITIN SHARMA</h2>
+        <p className='text-[#c2c2c2]'>- A passonate and dedicated front end developer, driven by the ever-evolving world of technology and its limitless possiblities.</p>
+        <div className='mt-3'>
+          <a href="/contact" className='font-semibold px-5 border-2 border-red-600 text-white rounded py-2'>Contact me</a>
+        </div>
+      </div>
+
+      <div className='border border-red-600'>
+
+      </div>
     </div>
   )
 }
